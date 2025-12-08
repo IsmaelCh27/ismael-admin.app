24107b3272e2e949812486e1b8098157d66c852b
Ismael Choque <ismael2.7cc@gmail.com>
2025-12-07 21:45:17 -0400

    fix: Enviroment adjustments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  supabaseUrl: 'https://wdwokqaxjbdlybcytlal.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indkd29rcWF4amJkbHliY3l0bGFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Nzk4MzcyNywiZXhwIjoyMDczNTU5NzI3fQ.NhLXxIMDbN1F7XzTuDfXTJlWHASsZr3yRqihkWlfwPU',
+  supabaseUrl: 'YOUR_SUPABASE_URL_HERE',
+  supabaseKey: 'YOUR_SUPABASE_KEY_HERE',
 };
